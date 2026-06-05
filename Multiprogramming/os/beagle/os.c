a76f43a59a447c2b2aeca644244bee84eee80975
EmilioCastellanos <22007232@galileo.edu>
2026-06-04 22:46:08 -0600

    feat(beagle): añadir soporte de traces y refactorizar SYS_YIELD

--- Multiprogramming/os/beagle/os.c
+++ Multiprogramming/os/beagle/os.c
@@ -8,6 +8,67 @@
 
 void os_timer_irq_handler(uint32_t *irq_frame);
 
+void os_trace_mode_switch(uint32_t pid, const char *direction, const char *reason, const char *extra) {
+    uart_puts("\nMODE_SWITCH ");
+    uart_puts(direction);
+    uart_puts(" pid=");
+    uart_putc((char)(pid + '0'));
+    uart_puts(" reason=");
+    uart_puts(reason);
+    if (extra != 0) {
+        uart_puts(" ");
+        uart_puts(extra);
+    }
+    uart_puts("\n");
+}
+
+static void trace_return(uint32_t syscall_num, int32_t rc) {
+    char extra[16];
+    pcb_t *current = scheduler_get_current_pcb();
+
+    extra[0] = 'i';
+    extra[1] = 'd';
+    extra[2] = '=';
+    extra[3] = (char)('0' + (char)syscall_num);
+    extra[4] = ' ';
+    extra[5] = 'r';
+    extra[6] = 'c';
+    extra[7] = '=';
+    if (rc < 0) {
+        extra[8] = '-';
+        extra[9] = (char)('0' + (char)(-rc));
+        extra[10] = '\0';
+    } else {
+        extra[8] = (char)('0' + (char)(rc % 10));
+        extra[9] = '\0';
+    }
+
+    os_trace_mode_switch(current->pid, "KERNEL_TO_USER", "syscall_return", extra);
+}
+
+void os_handle_sys_yield(uint32_t *frame) {
+    uint32_t usr_sp;
+    uint32_t usr_lr;
+    pcb_t *current_pcb;
+    pcb_t *next_pcb;
+
+    cpu_get_user_regs(&usr_sp, &usr_lr);
+    
+    current_pcb = scheduler_get_current_pcb();
+    pcb_save_from_irq_frame(current_pcb, frame, usr_sp, usr_lr);
+    if (current_pcb->state != PROC_TERMINATED) {
+        pcb_set_state(current_pcb, PROC_READY);
+    }
+
+    scheduler_next();
+
+    next_pcb = scheduler_get_current_pcb();
+    pcb_restore_to_irq_frame(next_pcb, frame, &usr_sp, &usr_lr);
+    cpu_set_user_regs(usr_sp, usr_lr);
+    pcb_set_state(next_pcb, PROC_RUNNING);
+    frame[0] = 0u;
+}
+
 static int32_t os_handle_sys_write(uint32_t fd, const char *buf, uint32_t len) {
     pcb_t *current = scheduler_get_current_pcb();
     uint32_t base = (current->pid == 1u) ? 0x82100000u : 0x82200000u;
@@ -33,15 +94,24 @@ static int32_t os_handle_sys_write(uint32_t fd, const char *buf, uint32_t len) {
 void os_syscall_dispatcher(uint32_t *frame) {
     uint32_t syscall_num = frame[0];
     uint32_t spsr = frame[14];
+    char extra[16];
+    int32_t rc = -1;
+    pcb_t *current = scheduler_get_current_pcb();
 
     if ((spsr & 0x1Fu) != 0x10u) {
+        uart_puts("\n[KERNEL] Syscall rejected: not from USR mode\n");
         frame[0] = 0xFFFFFFFFu;
         return;
     }
 
+    extra[0] = 'i'; extra[1] = 'd'; extra[2] = '=';
+    extra[3] = (char)('0' + (char)syscall_num);
+    extra[4] = '\0';
+    os_trace_mode_switch(current->pid, "USER_TO_KERNEL", "syscall", extra);
+
     if (syscall_num == 0u) {
-        os_timer_irq_handler(frame);
-        frame[0] = 0u;
+        os_handle_sys_yield(frame);
+        rc = 0;
     } else if (syscall_num == 1u) {
         uint32_t usr_sp;
         uint32_t usr_lr;
@@ -59,16 +129,24 @@ void os_syscall_dispatcher(uint32_t *frame) {
         pcb_restore_to_irq_frame(next_pcb, frame, &usr_sp, &usr_lr);
         cpu_set_user_regs(usr_sp, usr_lr);
         pcb_set_state(next_pcb, PROC_RUNNING);
-        frame[0] = 0u;
+        rc = 0;
     } else if (syscall_num == 2u) {
-        frame[0] = (uint32_t)os_handle_sys_write(frame[1], (const char *)frame[2], frame[3]);
+        rc = os_handle_sys_write(frame[1], (const char *)frame[2], frame[3]);
+        frame[0] = (uint32_t)rc;
     } else {
+        uart_puts("\n[KERNEL] Unknown syscall\n");
         frame[0] = 0xFFFFFFFFu;
+        rc = -1;
     }
+
+    trace_return(syscall_num, rc);
 }
 
 void os_fault_handler(uint32_t *frame) {
     (void)frame;
+    pcb_t *current = scheduler_get_current_pcb();
+    os_trace_mode_switch(current->pid, "USER_TO_KERNEL", "fault", "type=abort");
+
     scheduler_terminate_current();
 
     if (!scheduler_has_runnable()) {
@@ -80,6 +158,7 @@ void os_fault_handler(uint32_t *frame) {
     scheduler_next();
     {
         pcb_t *next_pcb = scheduler_get_current_pcb();
+        os_trace_mode_switch(next_pcb->pid, "KERNEL_TO_USER", "fault_recovery", 0);
         cpu_set_user_regs(next_pcb->sp, next_pcb->lr);
         pcb_set_state(next_pcb, PROC_RUNNING);
         __asm__ volatile (
@@ -101,12 +180,14 @@ void os_timer_irq_handler(uint32_t *irq_frame) {
     pcb_t *current_pcb;
     pcb_t *next_pcb;
 
+    current_pcb = scheduler_get_current_pcb();
+    os_trace_mode_switch(current_pcb->pid, "USER_TO_KERNEL", "timer_irq", 0);
+
     /* ACK timer + EOI so interrupts continue */
     timer_ack_interrupt();
 
     cpu_get_user_regs(&usr_sp, &usr_lr);
     
-    current_pcb = scheduler_get_current_pcb();
     pcb_save_from_irq_frame(current_pcb, irq_frame, usr_sp, usr_lr);
     if (current_pcb->state != PROC_TERMINATED) {
         pcb_set_state(current_pcb, PROC_READY);
@@ -118,6 +199,7 @@ void os_timer_irq_handler(uint32_t *irq_frame) {
     pcb_restore_to_irq_frame(next_pcb, irq_frame, &usr_sp, &usr_lr);
     cpu_set_user_regs(usr_sp, usr_lr);
     pcb_set_state(next_pcb, PROC_RUNNING);
+    os_trace_mode_switch(next_pcb->pid, "KERNEL_TO_USER", "dispatch", 0);
 }
 
 void os_kmain(void) {
@@ -133,6 +215,7 @@ void os_kmain(void) {
     uart_puts("[OS] jumping to P1\n");
 
     pcb_t *current = scheduler_get_current_pcb();
+    os_trace_mode_switch(current->pid, "KERNEL_TO_USER", "initial_launch", 0);
     cpu_switch_to_user_mode(current->pc, current->sp, current->lr);
 
     for (;;) {
